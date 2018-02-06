46b8a8dfca0471d1bf1694f1ca83f70a64791397
Venkat <venkat.repaka@gmail.com>
2018-02-06 23:45:02 +0530

    Lectures code

--- vehicle_detection/color_classify.py
+++ vehicle_detection/color_classify.py
@@ -1,11 +1,16 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from sklearn.preprocessing import StandardScaler
 import glob
+import time
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+# NOTE: the next import is only valid
+# for scikit-learn version <= 0.17
+# if you are using scikit-learn >= 0.18 then use this:
+from sklearn.model_selection import train_test_split
+# from sklearn.cross_validation import train_test_split
 
 
 # Define a function to compute binned color features
@@ -28,7 +33,6 @@ def color_hist(img, nbins=32, bins_range=(0, 256)):
     return hist_features
 
 
-###### TODO ###########
 # Define a function to extract features from a list of images
 # Have this function call bin_spatial() and color_hist()
 def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
@@ -61,15 +65,9 @@ def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
     return features
 
 
-# images = glob.glob('*.jpeg')
+# Read in car and non-car images
 cars = []
 notcars = []
-# for image in images:
-#     if 'image' in image or 'extra' in image:
-#         notcars.append(image)
-#     else:
-#         cars.append(image)
-
 car_images = glob.glob('../vehicles_smallset/**/*.jpeg')
 non_car_images = glob.glob('../non-vehicles_smallset/**/*.jpeg')
 for image in car_images:
@@ -77,31 +75,49 @@ for image in car_images:
 for image in non_car_images:
     notcars.append(image)
 
-car_features = extract_features(cars, cspace='RGB', spatial_size=(32, 32),
-                                hist_bins=32, hist_range=(0, 256))
-notcar_features = extract_features(notcars, cspace='RGB', spatial_size=(32, 32),
-                                   hist_bins=32, hist_range=(0, 256))
+# TODO play with these values to see how your classifier
+# performs under different binning scenarios
+spatial = 32
+histbin = 32
 
-if len(car_features) > 0:
-    # Create an array stack of feature vectors
-    X = np.vstack((car_features, notcar_features)).astype(np.float64)
-    # Fit a per-column scaler
-    X_scaler = StandardScaler().fit(X)
-    # Apply the scaler to X
-    scaled_X = X_scaler.transform(X)
-    car_ind = np.random.randint(0, len(cars))
-    # Plot an example of raw and scaled features
-    fig = plt.figure(figsize=(12, 4))
-    plt.subplot(131)
-    plt.imshow(mpimg.imread(cars[car_ind]))
-    plt.title('Original Image')
-    plt.subplot(132)
-    plt.plot(X[car_ind])
-    plt.title('Raw Features')
-    plt.subplot(133)
-    plt.plot(scaled_X[car_ind])
-    plt.title('Normalized Features')
-    fig.tight_layout()
-    plt.show()
-else:
-    print('Your function only returns empty feature vectors...')
+car_features = extract_features(cars, cspace='YUV', spatial_size=(spatial, spatial),
+                                hist_bins=histbin, hist_range=(0, 256))
+notcar_features = extract_features(notcars, cspace='YUV', spatial_size=(spatial, spatial),
+                                   hist_bins=histbin, hist_range=(0, 256))
+
+# Create an array stack of feature vectors
+X = np.vstack((car_features, notcar_features)).astype(np.float64)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=rand_state)
+
+# Fit a per-column scaler only on the training data
+X_scaler = StandardScaler().fit(X_train)
+# Apply the scaler to X_train and X_test
+X_train = X_scaler.transform(X_train)
+X_test = X_scaler.transform(X_test)
+
+print('Using spatial binning of:', spatial,
+      'and', histbin, 'histogram bins')
+print('Feature vector length:', len(X_train[0]))
+# Use a linear SVC
+svc = LinearSVC()
+# Check the training time for the SVC
+t = time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+print(round(t2 - t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+# Check the prediction time for a single sample
+t = time.time()
+n_predict = 10
+print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+print('For these', n_predict, 'labels: ', y_test[0:n_predict])
+t2 = time.time()
+print(round(t2 - t, 5), 'Seconds to predict', n_predict, 'labels with SVC')
