18ede02d5291d35721c29d04337951c97ae6f3c5
Venkat Repaka <venkat.repaka@gmail.com>
2018-02-13 21:53:25 +0530

    Improved classifier

--- vd/lectures_functions.py
+++ vd/lectures_functions.py
@@ -29,13 +29,13 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, featu
     if vis:
         features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
                                   cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True,
-                                  visualise=vis, feature_vector=feature_vec)
+                                  block_norm='L2', visualise=vis, feature_vector=feature_vec)
         return features, hog_image
     # Otherwise call with one output
     else:
         features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
                        cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True,
-                       visualise=vis, feature_vector=feature_vec)
+                       block_norm='L2', visualise=vis, feature_vector=feature_vec)
         return features
 
 
