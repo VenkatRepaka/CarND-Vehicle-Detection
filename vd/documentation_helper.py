8c60c0c646439d62cdddd489086f1556b3168167
Venkat Repaka <venkat.repaka@gmail.com>
2018-02-14 00:07:17 +0530

    Improved classifier

--- vd/documentation_helper.py
+++ vd/documentation_helper.py
@@ -8,6 +8,20 @@ from sklearn.preprocessing import StandardScaler
 import pickle
 
 
+color_space = 'YCrCb'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+hog_channel = "ALL"  # Can be 0, 1, 2, or "ALL"
+spatial_feat = True  # Spatial features on or off
+hist_feat = True  # Histogram features on or off
+hog_feat = True  # HOG features on or off
+dist_pickle = pickle.load(open("./model_params.p", "rb"))
+svc = dist_pickle["svc"]
+X_scaler = dist_pickle["scaler"]
+orient = dist_pickle["orient"]
+pix_per_cell = dist_pickle["pix_per_cell"]
+cell_per_block = dist_pickle["cell_per_block"]
+spatial_size = dist_pickle["spatial_size"]
+hist_bins = dist_pickle["hist_bins"]
+
 cars = glob.glob('../vehicles/**/*.png')
 non_cars = glob.glob('../non-vehicles/**/*.png')
 cars_index = random.randint(0, len(cars))
@@ -33,7 +47,7 @@ ax2.imshow(car_image_YCrCb)
 ax2.set_title('YCrCb Car Image', fontsize=10)
 ax3.plot(car_hist_features)
 ax3.set_title('Car Histogram', fontsize=10)
-ax4.imshow(cv2.resize(car_bin_spatial, (32, 32)))
+ax4.imshow(cv2.resize(car_image_YCrCb, (32, 32)))
 ax4.set_title('YCrCb Car Spatial (32, 32)', fontsize=10)
 ax5.plot(car_bin_spatial)
 ax5.set_title('YCrCb Car Spatial (32, 32) features', fontsize=10)
@@ -44,84 +58,75 @@ ax7.imshow(non_car_image_YCrCb)
 ax7.set_title('YCrCb Non Car Image', fontsize=10)
 ax8.plot(non_car_hist_features)
 ax8.set_title('Non Car Histogram', fontsize=10)
-ax9.imshow(cv2.resize(non_car_bin_spatial, (32, 32)))
+ax9.imshow(cv2.resize(non_car_image_YCrCb, (32, 32)))
 ax9.set_title('YCrCb Non Car Spatial (32, 32)', fontsize=10)
 ax10.plot(non_car_bin_spatial)
 ax10.set_title('YCrCb Non Car Spatial (32, 32) features', fontsize=10)
 plt.show()
 
-orient = 9
-pix_per_cell = 8
-cell_per_block = 2
-car_hog_features, car_hog_image = get_hog_features(cv2.cvtColor(car_image, cv2.COLOR_RGB2GRAY), orient, pix_per_cell,
-                                                   cell_per_block, vis=True, feature_vec=True)
-non_car_hog_features, non_car_hog_image = get_hog_features(cv2.cvtColor(non_car_image, cv2.COLOR_RGB2GRAY), orient,
-                                                           pix_per_cell, cell_per_block, vis=True, feature_vec=True)
-f, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(12, 6))
-ax1.imshow(car_image)
-ax1.set_title('Car Image', fontsize=10)
-ax2.imshow(car_hog_image, cmap='gray')
-ax2.set_title('Car Hog', fontsize=10)
-ax3.plot(car_hog_features)
-ax3.set_title('Car Hog Histogram', fontsize=10)
-ax4.imshow(non_car_image)
-ax4.set_title('Non Car Image', fontsize=10)
-ax5.imshow(non_car_hog_image, cmap='gray')
-ax5.set_title('Non Car Hog', fontsize=10)
-ax6.plot(non_car_hog_features)
-ax6.set_title('Non Car Hog Histogram', fontsize=10)
-plt.show()
 
-# Display scaled features
-
-color_space = 'YCrCb'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-orient = 9
-pix_per_cell = 8
-cell_per_block = 2
-hog_channel = "ALL"  # Can be 0, 1, 2, or "ALL"
-spatial_size = (16, 16)  # Spatial binning dimensions
-hist_bins = 32   # Number of histogram bins
-spatial_feat = True  # Spatial features on or off
-hist_feat = True  # Histogram features on or off
-hog_feat = True  # HOG features on or off
-
-car_features = extract_features(cars, color_space=color_space, spatial_size=spatial_size, hist_bins=hist_bins,
-                                orient=orient, pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                                hog_channel=hog_channel, spatial_feat=spatial_feat, hist_feat=hist_feat,
-                                hog_feat=hog_feat)
-notcar_features = extract_features(non_cars, color_space=color_space, spatial_size=spatial_size, hist_bins=hist_bins,
-                                   orient=orient, pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                                   hog_channel=hog_channel, spatial_feat=spatial_feat, hist_feat=hist_feat,
-                                   hog_feat=hog_feat)
-X = np.vstack((car_features, notcar_features)).astype(np.float64)
-X_scaler = StandardScaler().fit(X)
-scaled_X = X_scaler.transform(X)
-
-f, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(12, 6))
-ax1.imshow(car_image)
-ax1.set_title('Car Image', fontsize=10)
-ax2.plot(X[cars_index])
-ax2.set_title('Car Raw', fontsize=10)
-ax3.plot(scaled_X[cars_index])
-ax3.set_title('Car Scaled', fontsize=10)
-ax4.imshow(non_car_image)
-ax4.set_title('Non Car Image', fontsize=10)
-ax5.plot(X[len(cars) + non_cars_index - 1])
-ax5.set_title('Non Car Raw', fontsize=10)
-ax6.plot(scaled_X[len(cars) + non_cars_index - 1])
-ax6.set_title('Non Car Scaled', fontsize=10)
-plt.show()
-
-# Search windows
-dist_pickle = pickle.load(open("./model_params.p", "rb"))
-svc = dist_pickle["svc"]
-scales = [1, 1.3, 1.5, 1.8, 2, 2.4]
-# test_image_path = glob.glob('../test_images/test1.jpg')
-test_image = mpimg.imread('../test_images/test1.jpg')
-for scale in scales:
-    windows = find_cars(test_image, 400, 600, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
-                        hist_bins, vis=True)
-    image = draw_boxes(test_image, windows, thick=2)
-    plt.imshow(image)
-    plt.title('Scale ' + str(scale))
+def show_hog_features():
+    car_hog_features, car_hog_image = get_hog_features(cv2.cvtColor(car_image, cv2.COLOR_RGB2GRAY), orient, pix_per_cell,
+                                                       cell_per_block, vis=True, feature_vec=True)
+    non_car_hog_features, non_car_hog_image = get_hog_features(cv2.cvtColor(non_car_image, cv2.COLOR_RGB2GRAY), orient,
+                                                               pix_per_cell, cell_per_block, vis=True, feature_vec=True)
+    f, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(12, 6))
+    ax1.imshow(car_image)
+    ax1.set_title('Car Image', fontsize=10)
+    ax2.imshow(car_hog_image, cmap='gray')
+    ax2.set_title('Car Hog', fontsize=10)
+    ax3.plot(car_hog_features)
+    ax3.set_title('Car Hog Histogram', fontsize=10)
+    ax4.imshow(non_car_image)
+    ax4.set_title('Non Car Image', fontsize=10)
+    ax5.imshow(non_car_hog_image, cmap='gray')
+    ax5.set_title('Non Car Hog', fontsize=10)
+    ax6.plot(non_car_hog_features)
+    ax6.set_title('Non Car Hog Histogram', fontsize=10)
     plt.show()
+
+
+def show_scaled_features():
+    car_features = extract_features(cars, color_space=color_space, spatial_size=spatial_size, hist_bins=hist_bins,
+                                    orient=orient, pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                                    hog_channel=hog_channel, spatial_feat=spatial_feat, hist_feat=hist_feat,
+                                    hog_feat=hog_feat)
+    notcar_features = extract_features(non_cars, color_space=color_space, spatial_size=spatial_size, hist_bins=hist_bins,
+                                       orient=orient, pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                                       hog_channel=hog_channel, spatial_feat=spatial_feat, hist_feat=hist_feat,
+                                       hog_feat=hog_feat)
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)
+    X_scaler = StandardScaler().fit(X)
+    scaled_X = X_scaler.transform(X)
+
+    f, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(12, 6))
+    ax1.imshow(car_image)
+    ax1.set_title('Car Image', fontsize=10)
+    ax2.plot(X[cars_index])
+    ax2.set_title('Car Raw', fontsize=10)
+    ax3.plot(scaled_X[cars_index])
+    ax3.set_title('Car Scaled', fontsize=10)
+    ax4.imshow(non_car_image)
+    ax4.set_title('Non Car Image', fontsize=10)
+    ax5.plot(X[len(cars) + non_cars_index - 1])
+    ax5.set_title('Non Car Raw', fontsize=10)
+    ax6.plot(scaled_X[len(cars) + non_cars_index - 1])
+    ax6.set_title('Non Car Scaled', fontsize=10)
+    plt.show()
+
+
+def show_search_windows():
+    # Search windows
+    scales = [1, 1.3, 1.5, 1.8, 2, 2.4]
+    # test_image_path = glob.glob('../test_images/test1.jpg')
+    test_image = mpimg.imread('../test_images/test1.jpg')
+    for scale in scales:
+        windows = find_cars(test_image, 400, 600, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
+                            hist_bins, vis=True)
+        image = draw_boxes(test_image, windows, thick=2)
+        plt.imshow(image)
+        plt.title('Scale ' + str(scale))
+        plt.show()
+
+
+show_search_windows()
