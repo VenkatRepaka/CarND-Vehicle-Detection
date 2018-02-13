30abfa0959d284082ff540a03df360bc8db4630c
Venkat Repaka <venkat.repaka@gmail.com>
2018-02-13 12:27:18 +0530

    Improved classifier

--- vd/vehicle_detector.py
+++
@@ -1,7 +0,0 @@
-import collections
-
-
-class VehicleDetector:
-    def __init__(self, frames):
-        self.queued_boxes = collections.deque(maxlen=frames)
-        self.current_boxes = collections.deque(maxlen=frames)
